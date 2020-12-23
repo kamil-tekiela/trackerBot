0e4e1d0b8903439dd53ead551f7dcd8e24f88735
Dharman <tekiela246@gmail.com>
2020-12-23 22:48:55 +0000

    Update startTracker.php

--- public/startTracker.php
+++ public/startTracker.php
@@ -4,15 +4,26 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-exec('ps -eaf | grep \'[p]hp73 tracker.php\' | awk \'{print $2}\'', $out);
-foreach ($out as $pid) {
-	// exec("kill -9 $pid");
+$restart = isset($_GET['restart']);
+$stop = isset($_GET['stop']);
+
+exec('ps -eaf | grep \'[p]hp74 tracker.php\' | awk \'{print $2}\'', $out);
+foreach($out as $pid){
+	if($restart || $stop) {
+		exec("kill -9 $pid");
+	}
 }
-if ($out) {
+if($stop) {
+	echo 'It\'s probably stopped.';
+	die;
+}
+if($out && !$restart){
 	echo 'Already running';
 	die;
 }
-passthru('php73 tracker.php > trackerout.txt 2>trackerout.txt & ', $ret);
+
+// chdir('../../trackerBot/public');
+passthru('php74 tracker.php > trackerout.txt 2>trackerout.txt & ', $ret);
 
 var_dump($ret);
 
